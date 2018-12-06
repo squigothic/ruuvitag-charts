a15d78f2662fe8c16dddc5ad1fa0b6883d85d0b6
Teemu Laitila <squigothic@gmail.com>
2018-12-06 22:24:54 +0200

    static react frontend code now in /build

---
+++ build/precache-manifest.3314d85595b1229f268b5a716c2a5f56.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "07ab3f297e3089fa8e6e",
+    "url": "/static/js/main.07ab3f29.chunk.js"
+  },
+  {
+    "revision": "71712f6a10c792f4cbdf",
+    "url": "/static/js/1.71712f6a.chunk.js"
+  },
+  {
+    "revision": "b99781c414f245ddecebdb51ace2a3b3",
+    "url": "/index.html"
+  }
+];
