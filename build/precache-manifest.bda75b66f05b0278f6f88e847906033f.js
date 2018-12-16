2b65e70c3641d9aced264376bbb7bc5e4bf2e9c0
Teemu Laitila <squigothic@gmail.com>
2018-12-16 21:41:43 +0200

    centered the chart div

---
+++ build/precache-manifest.bda75b66f05b0278f6f88e847906033f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d712a7a38237bb19ecb0",
+    "url": "/static/js/main.d712a7a3.chunk.js"
+  },
+  {
+    "revision": "71712f6a10c792f4cbdf",
+    "url": "/static/js/1.71712f6a.chunk.js"
+  },
+  {
+    "revision": "d712a7a38237bb19ecb0",
+    "url": "/static/css/main.e5afe9f3.chunk.css"
+  },
+  {
+    "revision": "72789d19d6852a2fa7aa0f6176f0e2b2",
+    "url": "/index.html"
+  }
+];
