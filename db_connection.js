c94dcf349ecd1dd98352b2857394af775d86b154
Teemu <squigothic@gmail.com>
2019-04-23 23:02:17 +0300

    added conditional port for MYSQL

--- db_connection.js
+++ db_connection.js
@@ -2,13 +2,17 @@ require('dotenv').config()
 const mysql = require('mysql')
 
 // Connect to mysql database
+let MYSQL_PORT = process.env.MYSQL_PORT
+if (process.env.NODE_ENV === 'development') {
+  MYSQL_PORT = process.env.MYSQL_PORT_DEV
+}
 
 const connection = mysql.createConnection({
   host: process.env.MYSQL_URI,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PW,
   database: process.env.MYSQL_DB,
-  port: 9000
+  port: MYSQL_PORT
 })
 
 connection.connect()
---
+++ utils/config.js
@@ -0,0 +1,3 @@
+require('dotenv').config()
+
+let MYSQL_PORT_DEV = proce.env.MYSQL_PORT_DEV
