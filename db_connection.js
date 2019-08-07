4359bc6330997756d24502bc1b2b1637fac47b93
Teemu <squigothic@gmail.com>
2019-08-07 19:00:09 +0300

    modified db_connection to suit marakassi

--- db_connection.js
+++ db_connection.js
@@ -2,16 +2,25 @@ require('dotenv').config()
 const mysql = require('mysql')
 
 const config = {
+  host: process.env.MYSQL_URI,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PW,
   database: process.env.MYSQL_DB,
+  port: process.env.MYSQL_PORT
 }
 
 if (process.env.INSTANCE_CONNECTION_NAME && process.env.NODE_ENV === 'production') {
   config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`
 }
 
+if (process.env.NODE_ENV === 'development') {
+  config.port = process.env.MYSQL_PORT_DEV
+}
+
 const connection = mysql.createConnection(config)
+
+console.log('trying to connect: ', config)
+
 
 connection.connect((err) => {
   if (err) {
